94f0807c83c2c2e7df6772f0d675be2b6544fe53
Anna Bieliaieva <belyaevaan80@gmail.com>
2023-06-20 12:36:35 +0200

    fixed class drink and file drink.txt

--- src/main/java/ait/tr/models/Drink.java
+++ src/main/java/ait/tr/models/Drink.java
@@ -2,12 +2,16 @@ package ait.tr.models;
 
 public class Drink extends Food{
 
- private double size ;
- private boolean isAlcoholic;
 
-  public Drink(String title, double size, double price, double weight, boolean isVegan, boolean isForKids,
-      int calories) {
+ private boolean isAlcohol;
+
+  public Drink(String title, double price, double weight, boolean isVegan, boolean isForKids,
+      int calories, boolean isAlcohol) {
     super(title, price, weight, isVegan, isForKids, calories);
-    this.size = size;
+    this.isAlcohol = isAlcohol;
   }
+
+    public boolean isAlcohol() {
+        return isAlcohol;
+    }
 }
