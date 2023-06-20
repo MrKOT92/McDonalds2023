983201ecdf96757139402cdbd1730bae374043c2
Anna Bieliaieva <belyaevaan80@gmail.com>
2023-06-20 12:27:58 +0200

    add class OrderRepository

---
+++ src/main/java/ait/tr/repositories/OrderRepository.java
@@ -0,0 +1,14 @@
+package ait.tr.repositories;
+
+import ait.tr.models.Order;
+
+public class OrderRepository {
+
+    private String fileName;
+
+    public OrderRepository(String fileName) {
+        this.fileName = fileName;
+    }
+    //TODO realise
+    public void saveOrder(Order order){}
+}
