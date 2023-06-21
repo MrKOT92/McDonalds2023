b2c0f41977d62e22e83fc5229cb0ccef0112baef
Anna Bieliaieva <belyaevaan80@gmail.com>
2023-06-21 19:17:17 +0200

    add FoodServiceTest

---
+++ src/test/java/ait/tr/services/FoodServiceTest.java
@@ -0,0 +1,24 @@
+package ait.tr.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FoodServiceTest {
+
+    @Test
+    void chooseDrink() {
+    }
+
+    @Test
+    void chooseBurger() {
+    }
+
+    @Test
+    void chooseDessert() {
+    }
+
+    @Test
+    void finalMessage() {
+    }
+}
